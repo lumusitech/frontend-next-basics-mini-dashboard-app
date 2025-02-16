fa2bb1401e7518cee40a91ddb2db9ded140e8098
Luciano Figueroa <lumusitech@gmail.com>
2025-02-16 16:30:08 -0300

    feat: add side menu item component

---
+++ src/app/components/SidebarMenuItem.tsx
@@ -0,0 +1,25 @@
+import { JSX } from 'react';
+
+interface Props {
+  title: string;
+  subtitle: string;
+  path: string;
+  icon: JSX.Element;
+}
+
+export const SidebarMenuItem = ({ title, subtitle, path, icon }: Props) => {
+  return (
+    <a
+      href={path}
+      className="w-full px-2 inline-flex space-x-2 items-center border-b border-slate-700 py-3 bg-blue-800 hover:bg-white/5 transition ease-linear duration-150"
+    >
+      <div>{icon}</div>
+      <div className="flex flex-col">
+        <span className="text-lg font-bold leading-5 text-white">{title}</span>
+        <span className="text-sm text-white/50 hidden md:block">
+          {subtitle}
+        </span>
+      </div>
+    </a>
+  );
+};
