ed10039f94812e3c133063637f63a0a02d8524f1
Luciano Figueroa <lumusitech@gmail.com>
2025-02-16 22:04:24 -0300

    feat: add base pokemons page to dashboard

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image';
-import { IoBrowsersOutline, IoCalculator, IoLogoReact } from 'react-icons/io5';
+import {
+  IoBrowsersOutline,
+  IoCalculator,
+  IoFootball,
+  IoLogoReact,
+} from 'react-icons/io5';
 import { SidebarMenuItem } from './SidebarMenuItem';
 
 const menuItems = [
@@ -14,6 +19,12 @@ const menuItems = [
     subtitle: 'Client side counter',
     path: '/dashboard/counter',
     icon: <IoCalculator size={40} />,
+  },
+  {
+    title: 'Pokemons',
+    subtitle: 'static generation',
+    path: '/dashboard/pokemons',
+    icon: <IoFootball size={40} />,
   },
 ];
 
