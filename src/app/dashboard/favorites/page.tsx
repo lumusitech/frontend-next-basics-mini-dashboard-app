37c720b7165bbb25f94d987a9fc4f334e038040b
Luciano Figueroa <lumusitech@gmail.com>
2025-02-24 18:45:50 -0300

    feat: add simple widget into maon page

--- src/app/dashboard/favorites/page.tsx
+++ src/app/dashboard/favorites/page.tsx
@@ -9,7 +9,7 @@ export default async function PokemonsPage() {
   return (
     <div className="flex flex-col">
       <span className="text-5xl my-2">
-        Favorites Pokemons<small className="text-blue-500">Global State</small>
+        Favorites Pokemons <small className="text-blue-500">Global State</small>
       </span>
 
       <PokemonsGrid pokemons={[]} />
--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,7 +1,14 @@
+import { SimpleWidget } from '@/components';
+
 export default function MainPage() {
   return (
-    <div>
-      <h1>Main Page</h1>
+    <div className="text-black p-2">
+      <h1 className="text-3xl mt-2">Dashboard</h1>
+      <span className="text-xl">General info</span>
+
+      <div className="flex flex-wrap p-2 justify-center items-center">
+        <SimpleWidget />
+      </div>
     </div>
   );
 }
