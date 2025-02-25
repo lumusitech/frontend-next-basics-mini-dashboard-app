0ff40eb384d6789bb1de91093430931cb3da8083
Luciano Figueroa <lumusitech@gmail.com>
2025-02-25 00:47:41 -0300

    feat: add counter value from api to set initCounterState

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -12,13 +12,32 @@ interface Props {
   value?: number;
 }
 
+export interface CounterResponse {
+  method: string;
+  count: number;
+}
+
+const getApiCounter = async (): Promise<CounterResponse> => {
+  const data = await fetch('/api/counter').then((res) => res.json());
+  console.log({ data });
+
+  return data;
+};
+
 export const CartCounter = ({ value = 0 }: Props) => {
   const count = useAppSelector((state) => state.counter.count);
   const dispatch = useAppDispatch();
 
   // sync server state with redux cliente state
+  // useEffect(() => {
+  //   dispatch(initCounterState(value));
+  // }, []);
+
+  // Using Counter Api
   useEffect(() => {
-    dispatch(initCounterState(value));
+    getApiCounter().then(({ count }) => {
+      dispatch(initCounterState(count));
+    });
   }, []);
 
   return (
